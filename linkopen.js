e4b03cfc558bf55170f2bd9daba19a979b5c4966
8agus <merx570@gmail.com>
2023-05-04 12:15:24 +0400

    added hover and js link to certificate page baxes

---
+++ linkopen.js
@@ -0,0 +1,10 @@
+const cs50 = 'https://courses.edx.org/certificates/7b3996ef89404c198fbea6896f26b744'
+const cs = 'https://www.codecademy.com/profiles/system5215535901/certificates/5b5001a11d176d7acd8b2bf8'
+const css = 'https://www.codecademy.com/profiles/system5215535901/certificates/9a5bb1fc45b4281af1fffec93b0aaf05'
+const python = 'https://www.codecademy.com/profiles/system5215535901/certificates/6c152bd262967f8c941c9707ed636bda'
+const eHack = 'https://www.codecademy.com/profiles/system5215535901/certificates/03f04867616242888444dd22f20c29e5'
+const cl = 'https://www.codecademy.com/profiles/system5215535901/certificates/c87ba0541f8be78bc2f4ba1128233f6f'
+
+function openLink(url) {
+    window.open(url)
+}
